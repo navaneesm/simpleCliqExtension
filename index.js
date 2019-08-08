aeeec62d7e867105e2c3d8e81fc72a831d1f4527
navaneethakrishnan.sm <navaneethakrishnan.sm@zohocorp.com>
2019-08-08 13:16:54 +0530

    Updated Public Key
    * upated public key of urlFour csez extension
    * Decalred response obj

--- index.js
+++ index.js
@@ -25,19 +25,21 @@ app.post('/', function(req, res){
 	var body = req.body;
 	var signature = req.header('X-Cliq-Signature');
 	var publicKey = '-----BEGIN PUBLIC KEY-----\n'+
-			'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AM'+
-			'IIBCgKCAQEApLufF81FW87VuSwYRqZNEE'+
-			'XGoVyKx3TBvNbtAU0d5/P9Dwo/aStTsBt'+
-			'NxOhSOIyOI6ZJpogWaZh19iDYvcYyk1Oi'+
-			'GGM4Pl8UR0aCV4m5VJsGCkrWXSqtWFLuF'+
-			'G0v4iI+ASjeew8SDtk6+/2viiVpLLHUUl'+
-			'mfj1f+C7yFhiZQ0ubfG1dGMRBeuAH12tV'+
-			'hqGtnZJxMPWSO6O03qivmkPfoU65GcMAY'+
-			'p5uQCjYgtcxzLWYZsBUp3FEj6w4yygLjq'+
-			'LTE55hf4NbdjO6W3WI91Dfi1eEzTgfEkg'+
-			'TrcyF405FjPxhfM3mZyVQSMaGjaR3SdA2'+
-			'L0KsjGL3BWO0pc2ICggF3lQIDAQAB'+
-			'\n-----END PUBLIC KEY-----';
+					'MIIBIjANBgkqhkiG9w0BAQEFAAOC'+
+					'AQ8AMIIBCgKCAQEAlcHMHRBcRRnw'+
+					'hgNRsdw2CLdqonbI6S5lJhGOS/X/'+
+					'SeYc7IstRJXOL3KWkJFFzSb1HHQ8'+
+					'ajx+b5B14VoYd8PUDw6TdJrw45qm'+
+					'CJadMBhVIRtXcZEW6TE+fFGXtgs7'+
+					'qN8pb43h7op7OXa1AunzTDr5kx8l'+
+					'7u6ftCTObs+YMpGTPbqCpDxNgLqG'+
+					'7YKeWMAa55kg6B5DrlqsoCe+4KTh'+
+					'EUMdU6ETMIjELmpnJ67taD+mWkZ7'+
+					'H2PQwnmMxJC0nxY2QfJscR2hX4xc'+
+					'BbBlyFEOqVxh7tnGKqAuLVz3c/64'+
+					'AFCzfalRvoGukzv2VIWId5jUPfv6'+
+					'4ziahK12JgD1whutGGiQWwIDAQAB'+
+					'\n-----END PUBLIC KEY-----';
 	var verifier = crypto.createVerify('sha256');
 	verifier.update(JSON.stringify(body));
 	if(typeof signature !== "undefined")
@@ -45,7 +47,7 @@ app.post('/', function(req, res){
 		var result = verifier.verify(publicKey, signature, 'base64');
 		console.log("\n<!------- ******** Is valid signature # " + result + " # ******** -------!>");
 	}
-
+	var response = {};
 	response.output = {"text":"Hi Cliq URL invoke extension have reached the external server :smile:"};
 	console.log("\n<!------- ******** Execution Response ******** -------!>\n" + JSON.stringify(response) + "\n<!------- ******** End of Execution Response ******** -------!>");
   	res.send(response);
